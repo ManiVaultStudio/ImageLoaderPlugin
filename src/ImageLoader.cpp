f93eb10132c0705e98e7480486d2cc77f06fd8b8
Thomas Kroes <tkroes81@gmail.com>
2019-07-12 11:29:14 +0200

    First draft of image loading with FreeImage Pixels get loaded properly, but there are problems with PointsPlugin data population (TSNE does not run yet)

--- src/ImageLoader.cpp
+++ src/ImageLoader.cpp
@@ -1,6 +1,6 @@
 #include "ImageLoader.h"
 
-#include <FreeImage.h>
+#include <FreeImagePlus.h>
 
 #include "PointsPlugin.h"
 #include "Set.h"
@@ -30,73 +30,152 @@ void ImageLoader::init()
 
 void ImageLoader::loadData()
 {
-	/*
-    QString fileName = QFileDialog::getOpenFileName(Q_NULLPTR, "Load Image", "", "Image Files");
+    QString fileName = QFileDialog::getOpenFileName(Q_NULLPTR, "Load Image", "", "Image Files (*.bmp *.png *.jpg *jpeg)");
     
     // Don't try to load a file if the dialog was cancelled or the file name is empty
     if (fileName.isNull() || fileName.isEmpty())
         return;
-
-    qDebug() << "Loading image file: " << fileName;
 	
     QFile file(fileName);
 
     if (!file.open(QIODevice::ReadOnly | QIODevice::Text))
         throw DataLoadException(fileName, "File was not found at location.");
 	
-	// FreeImageNamespace::FREE_IMAGE_FORMAT formato = FreeImageNamespace::FreeImage_GetFileType(fileName.toUtf8(), 0);//Automatocally detects the format(from over 20 formats!)
-	
-	
-	FIBITMAP* imagen = FreeImage_Load(formato, "image.png");
-	
-	FIBITMAP* temp = imagen;
-	imagen = FreeImage_ConvertTo32Bits(imagen);
-	FreeImage_Unload(temp);
+	const auto convertToGrayscale = false;
 
-	int w = FreeImage_GetWidth(imagen);
-	int h = FreeImage_GetHeight(imagen);
+	fipImage image;
 
-    
-	std::vector<float> data;
-    int numDimensions = 1;
+	qDebug() << "Loading image file: " << fileName;
 
-    QTextStream in(&file);
-    while (!in.atEnd())
-    {
-        QString line = in.readLine();
-        QStringList tokens = line.split(",");
+	if (image.load(fileName.toUtf8())) {
+		const auto colorType = image.getColorType();
 
-        numDimensions = tokens.size();
-        for (int i = 0; i < tokens.size(); i++)
-        {
-            if (tokens[i].isEmpty())
-                continue;
-            float f = tokens[i].toFloat();
+		int noColorChannels = 0;
 
-            data.push_back(f);
-        }
-    }
+		switch (colorType)
+		{
+			case FIC_MINISBLACK:
+			case FIC_MINISWHITE:
+			case FIC_PALETTE: {
+				noColorChannels = 1;
+				break;
+			}
+			case FIC_RGB: {
+				noColorChannels = 3;
+				break;
+			}
+			case FIC_RGBALPHA:
+			case FIC_CMYK: {
+				noColorChannels = 4;
+				break;
+			}
+			default:
+				break;
+		}
 
-    bool ok;
-    QString dataSetName = QInputDialog::getText(nullptr, "Add New Dataset",
-        "Dataset name:", QLineEdit::Normal, "DataSet", &ok);
+		qDebug() << "Number of color channels: " << noColorChannels;
 
-    if (ok && !dataSetName.isEmpty()) {
-        QString name = _core->addData("Points", dataSetName);
-        const IndexSet& set = dynamic_cast<const IndexSet&>(_core->requestSet(name));
-        PointsPlugin& points = set.getData();
-        points.data.resize(data.size());
-        for (int i = 0; i < points.data.size(); i++) {
-            points.data[i] = data[i];
-        }
-        points.numDimensions = numDimensions;
-        qDebug() << "Number of dimensions: " << points.numDimensions;
+		if (convertToGrayscale) {
+			qDebug() << "Converting image to grayscale";
+			
+			image.convertToGrayscale();
 
-        _core->notifyDataAdded(name);
+			noColorChannels = 1;
+		}
 
-        qDebug() << "CSV file loaded. Num data points: " << points.data.size();
-    }
-	*/
+		bool converted = false;
+
+		if (noColorChannels == 1) {
+			image.convertToFloat();
+		}
+		else
+		{
+			switch (noColorChannels)
+			{
+				case 3: {
+					converted = image.convertToRGBF();
+					break;
+				}
+				case 4: {
+					converted = image.convertToRGBAF();
+					break;
+				}
+				default:
+					break;
+			}
+		}
+
+		if (converted) {
+			const auto imageWidth = image.getWidth();
+			const auto imageHeight = image.getHeight();
+
+			qDebug() << "Image dimensions: " << imageWidth << " x " << imageHeight;
+
+			const auto noPixels = imageWidth * imageHeight;
+
+			QFileInfo fileInfo(fileName);
+
+			QString dataSetName(fileInfo.fileName());
+
+			if (!dataSetName.isEmpty()) {
+				QString name = _core->addData("Points", dataSetName);
+
+				const IndexSet& set = dynamic_cast<const IndexSet&>(_core->requestSet(name));
+
+				PointsPlugin& points = set.getData();
+
+				std::vector<float> data;
+
+				const auto noDataElements = noPixels * noColorChannels;
+
+				data.resize(noDataElements);
+				points.data.resize(noDataElements);
+
+				unsigned x, y;
+
+				for (y = 0; y < imageHeight; y++) {
+					FIRGBAF* bits = (FIRGBAF*)image.getScanLine(y);
+					
+					for (x = 0; x < imageHeight; x++) {
+						const auto pixelIndex = y * imageWidth + x;
+
+						switch (noColorChannels)
+						{
+							case 1: {
+								data[pixelIndex] = bits[y].red;
+								break;
+							}
+							case 3: {
+								data[pixelIndex * 3 + 0] = bits[y].red;
+								data[pixelIndex * 3 + 1] = bits[y].green;
+								data[pixelIndex * 3 + 2] = bits[y].blue;
+								break;
+							}
+							case 4: {
+								data[pixelIndex * 4 + 0] = bits[y].red;
+								data[pixelIndex * 4 + 1] = bits[y].green;
+								data[pixelIndex * 4 + 2] = bits[y].blue;
+								data[pixelIndex * 4 + 3] = bits[y].alpha;
+								break;
+							}
+							default:
+								break;
+						}
+					}
+				}
+
+				for (int i = 0; i < points.data.size(); i++) {
+					points.data[i] = data[i];
+				}
+
+				points.numDimensions = noColorChannels;
+
+				_core->notifyDataAdded(name);
+			}
+		}
+
+		qDebug() << fileName << " loaded";
+	}
 }
 
 // =============================================================================
