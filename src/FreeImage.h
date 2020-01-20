fae81cc84b4fc8fdc52a4129e0333e5cf3dd10ba
Thomas <t.kroes@lumc.nl>
2020-01-20 10:14:25 +0100

    Documented freeimage wrapper class

--- src/FreeImage.h
+++ src/FreeImage.h
@@ -7,11 +7,38 @@ namespace fi {
 #include <FreeImage.h>
 }
 
+/**
+ * FreeImage class
+ * Wrapper class for FreeImage functionality
+ */
 class FreeImage
 {
 public:
+	/**
+	 * Load image from file path
+	 * @param imageFilePath Image file path
+	 * @return Handle to FreeImage bitmap
+	 */
 	static fi::FIBITMAP* freeImageLoad(const QString& imageFilePath);
+
+	/**
+	 * Open multi-bitmap (e.g. .tiff)
+	 * @param imageFilePath Image file path
+	 * @return Handle to FreeImage multi-bitmap
+	 */
 	static fi::FIMULTIBITMAP* freeImageOpenMultiBitmap(const QString& imageFilePath);
+
+	/**
+	 * Get number of pages in a FreeImage multi-bitmap
+	 * @param imageFilePath Image file path
+	 * @return Number of pages in FreeImage multi-bitmap
+	 */
 	static int freeImageGetPageCount(const QString& imageFilePath);
+
+	/**
+	 * Get size of a bitmap with FreeImage
+	 * @param imageFilePath Image file path
+	 * @return Size of the image
+	 */
 	static QSize freeImageGetSize(const QString& imageFilePath);
 };
