d6fd38e48d99a03a8af6a65e716c240b4faa07c4
Thomas <t.kroes@lumc.nl>
2020-01-20 11:56:57 +0100

    Documented the image loader class

--- src/ImageLoaderDialog.h
+++ src/ImageLoaderDialog.h
@@ -16,38 +16,58 @@ class ImageStackWidget;
 class MultiPartImageSequenceWidget;
 class ResampleImageSettingsWidget;
 
+/**
+ * Stacked widget class
+ * Helper class to ensure size hint
+ */
 class StackedWidget : public QStackedWidget
 {
 private:
+	/** Returns the hacked size hint */
 	QSize sizeHint() const override
 	{
 		return currentWidget()->sizeHint();
 	}
 
+	/** Returns the hacked minimum size hint */
 	QSize minimumSizeHint() const override
 	{
 		return currentWidget()->minimumSizeHint();
 	}
 };
 
+/**
+ * Image loader dialog class
+ * This class provides a user interface for loading high-dimensional image data
+ */
 class ImageLoaderDialog : public QDialog
 {
 public:
+	/**
+	 * Constructor
+	 * @param imageLoaderPlugin Pointer to image loader plugin class
+	 */
 	ImageLoaderDialog(ImageLoaderPlugin* imageLoaderPlugin);
+
+	/** Destructor */
 	~ImageLoaderDialog() override;
 
 private:
+	/**
+	 * Invoked when a message is received
+	 * @param message Received message
+	 */
 	void onMessage(const QString& message);
 
 private:
-	QSettings						_settings;							/*!< Settings */
-	ImageLoaderPlugin*				_imageLoaderPlugin;					/*!< Image loader plugin */
-	QVBoxLayout*					_mainLayout;						/*!< Main UI layout */
-	QVBoxLayout*					_settingsLayout;					/*!< Settings layout */
-	QComboBox*						_typesComboBox;						/*!< Loader types combobox e.g. sequence, stack */
-	StackedWidget*					_pagesStackedWidget;				/*!< Stacked pages widget */
-	ImageSequenceWidget*			_imageSequenceWidget;				/*!< Image sequence widget */
-	ImageStackWidget*				_imageStackWidget;					/*!< Image stack widget */
-	MultiPartImageSequenceWidget*	_multiPartImageSequenceWidget;		/*!< Multi-part image sequence widget */
-	QStatusBar*						_statusBar;							/*!< Status bar widget */
+	QSettings						_settings;							/** Settings */
+	ImageLoaderPlugin*				_imageLoaderPlugin;					/** Image loader plugin */
+	QVBoxLayout*					_mainLayout;						/** Main UI layout */
+	QVBoxLayout*					_settingsLayout;					/** Settings layout */
+	QComboBox*						_typesComboBox;						/** Loader types combobox e.g. sequence, stack */
+	StackedWidget*					_pagesStackedWidget;				/** Stacked pages widget */
+	ImageSequenceWidget*			_imageSequenceWidget;				/** Image sequence widget */
+	ImageStackWidget*				_imageStackWidget;					/** Image stack widget */
+	MultiPartImageSequenceWidget*	_multiPartImageSequenceWidget;		/** Multi-part image sequence widget */
+	QStatusBar*						_statusBar;							/** Status bar widget */
 };
