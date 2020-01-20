7a6417b448e76a3b29bfa368979a2a092c8b919b
Thomas <t.kroes@lumc.nl>
2020-01-20 12:04:51 +0100

    Documented the image loader plugin class

--- src/ImageLoaderPlugin.h
+++ src/ImageLoaderPlugin.h
@@ -10,34 +10,46 @@
 using hdps::plugin::LoaderPluginFactory;
 using hdps::plugin::LoaderPlugin;
 
-/*!
-	\class ImageLoaderPlugin
-	\inherits LoaderPlugin
-	\brief Abstract base scanner class for scanning images
-	\ingroup ImageLoaderPlugin
-*/
+/**
+ * Image loader plugin class
+ * This loader plugin class provides functionality to load high-dimensional image data into HDPS
+ */
 class ImageLoaderPlugin : public LoaderPlugin
 {
 public:
+	/** Default constructor */
 	ImageLoaderPlugin();
     
+	/** Initializes the plugin */
     void init() override;
 
+	/** Load high dimensional image data */
     void loadData() Q_DECL_OVERRIDE;
 
+	/**
+	 * Add images to HDPS
+	 * @param payload High dimensional image data payload
+	 */
 	void addImages(std::shared_ptr<Payload> payload);
 };
 
+/**
+ * Image loader plugin factory class
+ * A factory for creating image loader plugin instances
+ */
 class ImageLoaderPluginFactory : public LoaderPluginFactory
 {
 	Q_INTERFACES(hdps::plugin::LoaderPluginFactory hdps::plugin::PluginFactory)
-    Q_OBJECT
-    Q_PLUGIN_METADATA(IID   "nl.tudelft.ImageLoaderPlugin"
-                      FILE  "ImageLoaderPlugin.json")
-    
+		Q_OBJECT
+		Q_PLUGIN_METADATA(IID   "nl.tudelft.ImageLoaderPlugin" FILE  "ImageLoaderPlugin.json")
+
 public:
+	/** Default constructor */
 	ImageLoaderPluginFactory(void) {}
-    ~ImageLoaderPluginFactory(void) override {}
-    
+
+	/** Destructor */
+	~ImageLoaderPluginFactory(void) override {}
+
+	/** Creates an image loader plugin instance */
 	LoaderPlugin* produce() override;
 };
