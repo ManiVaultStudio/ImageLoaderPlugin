3d30f8fe7a718aedfbf02a8395ec718827ee26a0
Thomas <t.kroes@lumc.nl>
2020-01-20 13:14:25 +0100

    Documented the settings class

--- src/Settings.h
+++ src/Settings.h
@@ -2,22 +2,39 @@
 
 #include <QSettings>
 
-/*!
-	\class Settings
-	\brief Wrapper interface for interfacing with QSettings
-	\ingroup ImageLoaderPlugin
-*/
+/**
+ * Settings class
+ * Convenience class for serializing persistent image loader/scanner and UI parameters
+ */
 class Settings
 {
 public:
+	/**
+	 * Constructor
+	 * @param organization Organization name
+	 * @param application Application name
+	 * @param prefix Prefix name
+	 */
 	Settings(const QString& organization, const QString& application, const QString& prefix);
 
+	/** Returns the prefix */
 	QString prefix() const;
 
+	/**
+	 * Retrieves a setting
+	 * @param name Name of the setting
+	 * @param defaultValue Default value of the setting
+	 */
 	QVariant setting(const QString& name, const QVariant& defaultValue = QVariant());
+
+	/**
+	 * Sets a setting
+	 * @param name Name of the setting
+	 * @param value Setting value
+	 */
 	void setSetting(const QString& name, const QVariant& value);
 
 private:
-	QSettings	_settings;		/*!< Wrapped QSettings object */
-	QString		_prefix;		/*!< Settings prefix */
+	QSettings	_settings;		/** Wrapped QSettings object */
+	QString		_prefix;		/** Settings prefix */
 };
