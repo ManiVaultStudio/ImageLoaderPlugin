edeb6bad829fcacb8cbe937362ee14857f3c38b3
Thomas <t.kroes@lumc.nl>
2020-01-21 09:09:57 +0100

    Documented image sequence widget class

--- src/ImageSequenceWidget.h
+++ src/ImageSequenceWidget.h
@@ -13,28 +13,34 @@ namespace Ui {
 
 class ImageLoaderPlugin;
 
-/*!
-	\class ImageSequenceWidget
-	\inherits QWidget
-	\brief Provides a UI widget for interfacing with \a scanner and \a loader parameters
-	\relates ImageSequenceScanner
-	\relates ImageLoader
-	\ingroup ImageLoaderPlugin
-*/
+/**
+ * Image sequence widget
+ * User interface widget for image sequence loader/scanner settings
+ */
 class ImageSequenceWidget : public QWidget
 {
 	Q_OBJECT
 
 public:
+	/**
+	 * Constructor
+	 * @param imageLoaderPlugin Pointer to image loader plugin
+	 */
 	ImageSequenceWidget(ImageLoaderPlugin* imageLoaderPlugin);
+
+	/** Destructor */
     ~ImageSequenceWidget();
 
 signals:
+	/**
+	 * Broadcasts a message
+	 * @param message Message to broadcast
+	 */
 	void message(const QString& message);
 
 private:
-	ImageLoaderPlugin*							_imageLoaderPlugin;		/*!< Pointer to the ImageLoaderPlugin */
-	std::unique_ptr<Ui::ImageSequenceWidget>	_ui;					/*!< Externally loaded UI */
-	ImageSequenceScanner						_scanner;				/*!< Scanner */
-	ImageLoader									_loader;				/*!< Loader */
+	ImageLoaderPlugin*							_imageLoaderPlugin;		/** Pointer to the ImageLoaderPlugin */
+	std::unique_ptr<Ui::ImageSequenceWidget>	_ui;					/** Externally loaded UI */
+	ImageSequenceScanner						_scanner;				/** Scanner */
+	ImageLoader									_loader;				/** Loader */
 };
