00506e905a3c6b1c57cdfaedd050a1e8f4fbd0b7
Thomas <t.kroes@lumc.nl>
2020-01-21 09:14:44 +0100

    Documented image stack widget class

--- src/ImageSequenceWidget.h
+++ src/ImageSequenceWidget.h
@@ -29,7 +29,7 @@ public:
 	ImageSequenceWidget(ImageLoaderPlugin* imageLoaderPlugin);
 
 	/** Destructor */
-    ~ImageSequenceWidget();
+	~ImageSequenceWidget();
 
 signals:
 	/**
--- src/ImageStackWidget.h
+++ src/ImageStackWidget.h
@@ -15,31 +15,41 @@ class ImageLoaderPlugin;
 
 class QEvent;
 
-/*!
-	\class ImageStackWidget
-	\inherits QWidget
-	\brief Provides a UI widget for interfacing with \a scanner and \a loader parameters
-	\relates ImageStackScanner
-	\relates ImageLoader
-	\ingroup ImageLoaderPlugin
-*/
+/**
+ * Image stack widget
+ * User interface widget for image stack loader/scanner settings
+ */
 class ImageStackWidget : public QWidget
 {
 	Q_OBJECT
 
 public:
+	/**
+	 * Constructor
+	 * @param imageLoaderPlugin Pointer to image loader plugin
+	 */
 	ImageStackWidget(ImageLoaderPlugin* imageLoaderPlugin);
-    ~ImageStackWidget();
+
+	/** Destructor */
+	~ImageStackWidget();
 
 private:
+	/**
+	 * Invoked when the widget is show on the screen
+	 * @param showEvent Show event
+	 */
 	void showEvent(QShowEvent* showEvent);
 
 signals:
+	/**
+	 * Broadcasts a message
+	 * @param message Message to broadcast
+	 */
 	void message(const QString& message);
 
 private:
-	ImageLoaderPlugin*						_imageLoaderPlugin;		/*!< Pointer to the ImageLoaderPlugin */
-	std::unique_ptr<Ui::ImageStackWidget>	_ui;					/*!< Externally loaded UI */
-	ImageStackScanner						_scanner;				/*!< Scanner */
-	ImageLoader								_loader;				/*!< Loader */
+	ImageLoaderPlugin*						_imageLoaderPlugin;		/** Pointer to the ImageLoaderPlugin */
+	std::unique_ptr<Ui::ImageStackWidget>	_ui;					/** Externally loaded UI */
+	ImageStackScanner						_scanner;				/** Scanner */
+	ImageLoader								_loader;				/** Loader */
 };
