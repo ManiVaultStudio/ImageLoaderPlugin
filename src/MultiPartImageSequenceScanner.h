33cb40c1ec0d7048cba608e2438a3a0e18a98b84
Thomas <t.kroes@lumc.nl>
2020-01-21 09:16:45 +0100

    Documented multi-part image stack scanner class

--- src/MultiPartImageSequenceScanner.h
+++ src/MultiPartImageSequenceScanner.h
@@ -2,24 +2,27 @@
 
 #include "ImageScanner.h"
 
-#include <QObject>
-#include <QString>
-
-/*!
-	\class MultiPartImageSequenceScanner
-	\inherits ImageScanner
-	\brief Scans for multi-part image stacks
-	\ingroup ImageLoaderPlugin
-*/
+/**
+ * Image stack scanner class
+ * This class provides functionality for discovering multi-part image stacks
+ */
 class MultiPartImageSequenceScanner : public ImageScanner {
 	Q_OBJECT
 
 public:
+	/** Default constructor */
 	MultiPartImageSequenceScanner();
 
+	/** Scan for multi-part image stacks */
 	void scan() override;
+
+	/** Start scan thread */
 	void run() override;
 
 private:
+	/**
+	 * Scan directory
+	 * @param directory Search directory
+	 */
 	void scanDir(const QString& directory);
 };
