144ceb2dd125731a8e2cce38effe3e9d494ac1b8
Alexander Vieth <a.vieth@tudelft.nl>
2024-03-04 17:08:30 +0100

    Change some hdps references to manivault

--- conanfile.py
+++ conanfile.py
@@ -14,17 +14,17 @@ class ImageLoaderPluginConan(ConanFile):
     """Class to package ImageLoaderPlugin using conan
 
     Packages both RELEASE and DEBUG.
-    Uses rules_support (github.com/hdps/rulessupport) to derive
+    Uses rules_support (github.com/ManiVaultStudio/rulessupport) to derive
     versioninfo based on the branch naming convention
-    as described in https://github.com/hdps/core/wiki/Branch-naming-rules
+    as described in https://github.com/ManiVaultStudio/core/wiki/Branch-naming-rules
     """
 
     name = "ImageLoaderPlugin"
     description = (
-        "A plugin for viewing image data in the high-dimensional plugin system (HDPS)."
+        "A plugin for viewing image data ManiVaultStudio."
     )
-    topics = ("hdps", "plugin", "image data", "loading")
-    url = "https://github.com/hdps/ImageLoaderPlugin"
+    topics = ("hdps", "ManiVaultStudio", "plugin", "image data", "loading")
+    url = "https://github.com/ManiVaultStudio/ImageLoaderPlugin"
     author = "B. van Lew b.van_lew@lumc.nl"  # conan recipe author
     license = "MIT"
 
