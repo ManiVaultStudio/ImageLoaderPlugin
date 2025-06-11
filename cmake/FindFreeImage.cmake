2b6a9d89b4f9b08697e9625d6fcd314e9c5664d2
Sreeparna Deb <>
2025-06-11 14:02:07 +0200

    macOS build: updates for rpath and homebrew FreeImage

--- cmake/FindFreeImage.cmake
+++ cmake/FindFreeImage.cmake
@@ -8,15 +8,21 @@ if(TARGET FreeImage)
     message(STATUS "Found FreeImage")
     message(STATUS "  Target : FreeImage")
 else()
+    set(FREEIMAGE_CHECK_INCLUDE_DIRS)
+    set(FREEIMAGE_CHECK_LIBRARY_DIRS)
+    if(FREEIMAGE_INCLUDE_DIR_HINTS)
+        list(APPEND FREEIMAGE_CHECK_INCLUDE_DIRS ${FREEIMAGE_INCLUDE_DIR_HINTS})
+    endif()
     list(APPEND FREEIMAGE_CHECK_INCLUDE_DIRS
-        ${FREEIMAGE_INCLUDE_DIR_HINTS}
         /usr/include 
         /usr/local/include 
         /opt/homebrew/include
     )
 
+    if(FREEIMAGE_LIBRARY_DIR_HINTS)
+        list(APPEND FREEIMAGE_CHECK_LIBRARY_DIRS ${FREEIMAGE_LIBRARY_DIR_HINTS})
+    endif()
     list(APPEND FREEIMAGE_CHECK_LIBRARY_DIRS
-        ${FREEIMAGE_LIBRARY_DIR_HINTS}
         /usr/lib 
         /usr/local/lib 
         /opt/homebrew/lib
@@ -27,28 +33,28 @@ else()
         FreeImage.h
         PATHS 
         ${FREEIMAGE_CHECK_INCLUDE_DIRS})
-    )
 
     find_library(FreeImage_LIBRARIES
         NAMES 
         freeimage
         PATHS 
         ${FREEIMAGE_CHECK_LIBRARY_DIRS})
-    )
 
-    if(FREEIMAGE_INCLUDE_DIRS AND FREEIMAGE_LIBRARIES)
+    if(FreeImage_INCLUDE_DIRS AND FreeImage_LIBRARIES)
         set(FREEIMAGE_FOUND TRUE)
 
-        add_library(FreeImage INTERFACE IMPORTED)
-        target_include_directories(
-            FreeImage INTERFACE ${FREEIMAGE_INCLUDE_DIRS})
-        target_link_libraries(
-            FreeImage INTERFACE ${FREEIMAGE_LIBRARIES})
+        add_library(FreeImage SHARED IMPORTED)
+        set_target_properties(FreeImage PROPERTIES
+        IMPORTED_LOCATION "${FreeImage_LIBRARIES}"
+        INTERFACE_INCLUDE_DIRECTORIES "${FreeImage_INCLUDE_DIRS}"
+        )
 
         message(STATUS "Found FreeImage")
         message(STATUS "  Includes : ${FREEIMAGE_INCLUDE_DIRS}")
         message(STATUS "  Libraries : ${FREEIMAGE_LIBRARIES}")
     else()
-         message(FATAL_ERROR "Could not find FreeImage")
+        message(WARNING "Searched include dirs: ${FREEIMAGE_CHECK_INCLUDE_DIRS}")
+        message(WARNING "Searched lib dirs: ${FREEIMAGE_CHECK_LIBRARY_DIRS}")
+        message(FATAL_ERROR "Could not find FreeImage")
     endif()
 endif()
